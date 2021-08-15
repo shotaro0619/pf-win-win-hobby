c20ebe0323d39e232b69053d0ff5d291825b05d7
shotaro0619 <66shotaro99@gmail.com>
2021-08-15 05:57:57 +0000

    [fix]お問い合わせ機能レイアウト

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+  validates :name, presence: true, length: { in: 2..20 }
+  validates :email, :phone_number, :message, presence: true
+  validates :phone_number, numericality: { only_integer: true }
 end
