51be092137eb23dbe58466c5f3871c4ba93d2a7d
shotaro0619 <66shotaro99@gmail.com>
2021-09-07 13:52:57 +0000

    [Add]通知機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,13 +3,12 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
-    # redirect_to request.referer
+    @user.create_notification_follow!(current_user)
   end
 
   def destroy
     @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
-    # redirect_to request.referer
   end
 #————————フォロー・フォロワー一覧を表示する-————————————
   def followings
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+  
+  has_many :notifications, dependent: :destroy
 end
