81e670e7ef54fb38b7ac043e8a5727d7a610aafa
shotaro0619 <66shotaro99@gmail.com>
2021-09-13 07:41:12 +0000

    [Fix]通知機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,10 +19,10 @@ class MessagesController < ApplicationController
   def create
         if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
           @message = Message.new
+          Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
           # ここから
           @room_notification=@message.room
           @room = Room.find(params[:message][:room_id])
-          Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
           @messages = @room.messages
           # ここまでを追加
           if @message.save
